c0e0889800a706758281532389c2d522d30fa331
黄先生 <1608626145@qq.com>
2026-04-08 14:38:05 +0800

    优化细节

--- stc/database/20260401000001_install_plugin_backup_record.php
+++ stc/database/20260401000001_install_plugin_backup_record.php
@@ -22,16 +22,56 @@ class InstallPluginBackupRecord extends Migrator
         ]);
 
         PhinxExtend::upgrade($table, [
-            ['name', 'string', ['limit' => 255, 'default' => '', 'null' => true, 'comment' => '备份名称']],
-            ['file', 'string', ['limit' => 500, 'default' => '', 'null' => true, 'comment' => '备份文件名']],
-            ['size', 'biginteger', ['limit' => 20, 'default' => 0, 'null' => true, 'comment' => '备份文件大小(字节)']],
-            ['path', 'string', ['limit' => 500, 'default' => '', 'null' => true, 'comment' => '备份文件路径']],
-            ['tables', 'integer', ['limit' => 5, 'default' => 0, 'null' => true, 'comment' => '备份表数量']],
-            ['status', 'integer', ['limit' => 1, 'default' => 1, 'null' => true, 'comment' => '状态(0失败,1成功)']],
-            ['storage_type', 'string', ['limit' => 20, 'default' => '', 'null' => true, 'comment' => '存储类型(空=本地,alioss/qiniu等=对象存储)']],
-            ['storage_url', 'string', ['limit' => 500, 'default' => '', 'null' => true, 'comment' => '对象存储访问地址']],
-            ['create_at', 'datetime', ['default' => null, 'null' => true, 'comment' => '创建时间']],
-            ['update_at', 'datetime', ['default' => null, 'null' => true, 'comment' => '更新时间']],
+            [
+                'name',
+                'string',
+                ['limit' => 255, 'default' => '', 'null' => true, 'comment' => '备份名称']
+            ],
+            [
+                'file',
+                'string',
+                ['limit' => 500, 'default' => '', 'null' => true, 'comment' => '备份文件名']
+            ],
+            [
+                'size',
+                'biginteger',
+                ['limit' => 20, 'default' => 0, 'null' => true, 'comment' => '备份文件大小(字节)']
+            ],
+            [
+                'path',
+                'string',
+                ['limit' => 500, 'default' => '', 'null' => true, 'comment' => '备份文件路径']
+            ],
+            [
+                'tables',
+                'integer',
+                ['limit' => 5, 'default' => 0, 'null' => true, 'comment' => '备份表数量']
+            ],
+            [
+                'status',
+                'integer',
+                ['limit' => 1, 'default' => 1, 'null' => true, 'comment' => '状态(0失败,1成功)']
+            ],
+            [
+                'storage_type',
+                'string',
+                ['limit' => 20, 'default' => '', 'null' => true, 'comment' => '存储类型(空=本地,alioss/qiniu等=对象存储)']
+            ],
+            [
+                'storage_url',
+                'string',
+                ['limit' => 500, 'default' => '', 'null' => true, 'comment' => '对象存储访问地址']
+            ],
+            [
+                'create_at',
+                'datetime',
+                ['default' => null, 'null' => true, 'comment' => '创建时间']
+            ],
+            [
+                'update_at',
+                'datetime',
+                ['default' => null, 'null' => true, 'comment' => '更新时间']
+            ],
         ], [
             'status',
             'storage_type',
